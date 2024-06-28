dd888cdcc73b9b155961b05ab5d177891e11cd77
Vivek Sharma <viveksharma02005@gmail.com>
2024-06-28 10:01:36 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code in new version
+
+// alert("hello")  // not run beacuse we are using node js not browser
+console.log(3+3);
+
+let istrue = false
+let name = "javaScript"
+
+
+// primitive datatypes in js 
+//number = 2 power 52
+//boolean = true/false
+//bigInt 
+//String = ""
+//null = standalone value
+//undefined 
+//symbol
+
+console.log( typeof null);
