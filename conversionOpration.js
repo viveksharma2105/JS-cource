78d54201d35022663c91612dae3d9740d9ac6bf2
Vivek Sharma <viveksharma02005@gmail.com>
2024-06-28 12:03:12 +0530

    conversion

---
+++ conversionOpration.js
@@ -0,0 +1,24 @@
+let score = "21a"
+
+
+
+console.log(typeof score);
+console.log(typeof (score))
+
+let type = Number(score)
+console.log(typeof type)
+console.log(type)
+
+/*
+"33" = 33
+"33ab" = NaN
+true = 1
+false = 0
+"" = false
+"vivek" = true 
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
