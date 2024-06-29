91c7706700fe4717129a84c59cd9c01643cf449f
Vivek Sharma <viveksharma02005@gmail.com>
2024-06-29 08:51:46 +0530

    conversion+oprations

--- conversionOpration.js
+++ conversionOpration.js
@@ -22,3 +22,17 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber)
+console.log("-------------oprations----------")
+
+//***************************************OPRATIONS*********************************************** */
+let value = 5
+let negval = -value
+console.log(negval)
+//console.log(2**3) = 2^3(power)
+console.log("2"+ 1)
+console.log( 2 + "1")
+console.log( 2 + 1 + "1")
+console.log(+true) // tricky conversion val = 1
+console.log(+"")   // val = 0
+
+
