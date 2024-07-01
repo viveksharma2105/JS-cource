cae438bd7993ca0b5856213e5f6ee0533a76f862
Vivek Sharma <viveksharma02005@gmail.com>
2024-07-01 15:07:24 +0530

    datatype

--- datatype.js
+++ datatype.js
@@ -7,7 +7,7 @@ let istrue = false
 let name = "javaScript"
 
 
-// primitive datatypes in js 
+// -------------primitive datatypes in js -------------
 //number = 2 power 52
 //boolean = true/false
 //bigInt 
@@ -16,4 +16,35 @@ let name = "javaScript"
 //undefined 
 //symbol
 
+const score = 33
+const score1 = 33.5
+
+const isLoggin = false
+
+const outSideTemp = null
+let email;
+
+const id = Symbol('123')
+const id1 = Symbol('123') //although they have same value but in result they have unique value
+
+console.log(id === id1 ); //check
+
 console.log( typeof null);
+
+
+
+//----------------non primitive----------------------
+//array
+//objects
+//functions
+
+const heros = ["Ironman" , "Shaktiman"]; //array
+
+let myobj = {
+    name: "vivek",
+    age: 19,
+}
+
+const myfun =  function(){
+    console.log("hellow");
+}
