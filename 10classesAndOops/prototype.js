98214fdf274c057396165f5b241734b66d322080
Vivek Sharma <viveksharma02005@gmail.com>
2024-08-01 22:50:49 +0530

    prototype

---
+++ 10classesAndOops/prototype.js
@@ -0,0 +1,12 @@
+// let myname = 'vivek'
+// let age = 17
+
+// console.log(myname.trueLength);
+
+const arr = ["thor", "spiderman"]
+ 
+
+Object.prototype.vivek = function(){
+    console.log(`vivek in all obj`);
+    
+}
