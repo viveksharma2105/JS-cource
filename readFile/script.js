9435b2eb1ae3b8bb5bd2be15a6eed95834d64744
Vivek Sharma <viveksharma02005@gmail.com>
2024-08-18 09:50:49 +0530

    file read

--- readFile/script.js
+++ readFile/script.js
@@ -2,7 +2,7 @@
 const fs = require('fs');  //this is how we import file library
 
 
-// this is synchornously:- means all code executing one-by-one 
+// this is synchornously:- means all code executing one-by-one ---------------------------
 const data = fs.readFileSync('a.txt', 'utf8');
 const data1 = fs.readFileSync('b.txt', 'utf8');
 console.log(data);
@@ -10,12 +10,17 @@ console.log(data1);
 
 
 
-//Asynchornously code:- means all code executing simuntaniously 
+
+//Asynchornously code:- means all code executing simuntaniously----------------------------
 //here done print first because while  functions calling "done" is loged .
-
-
 function print(err,data){
-    console.log(data);
+    if(err){
+        console.log("File not Found");
+        
+    }else{
+        console.log(data);
+
+    }
     
 }
 fs.readFile('a.txt', 'utf8', print);
